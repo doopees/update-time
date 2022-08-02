3a252f42604797cbdae67f2f5948bee40537434f
joan <59487773+doopees@users.noreply.github.com>
2022-08-02 15:27:16 -0500

    Update update_time.py

--- update_time.py
+++ update_time.py
@@ -13,4 +13,4 @@ date_fmt = '%Y-%m-%dT%X.%f-05:00'
 date_obj = datetime.strptime(date_str, date_fmt)
 
 shell_cmd = date_obj.strftime('sudo date -s "%a %b %d %X -05 %Y"')
-#os.system(shell_cmd)
+os.system(shell_cmd)
