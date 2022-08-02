64277ec4e769a4914790188310f9c5042458cfa5
joan <59487773+doopees@users.noreply.github.com>
2022-08-02 17:24:57 -0500

    Update update_time.py

--- update_time.py
+++ update_time.py
@@ -3,17 +3,31 @@
 # A symbolic link to this file was created in /bin/
 # and @reboot /bin/update_time.py & was added to crontab.
 
+
 import os
 import json
 import commands
 from datetime import datetime
 
-_, response = commands.getstatusoutput('curl -s http://worldtimeapi.org/api/timezone/America/Lima')
-api_data = json.loads(response)
 
-date_str = api_data['datetime']
-date_fmt = '%Y-%m-%dT%X.%f-05:00'
-date_obj = datetime.strptime(date_str, date_fmt)
+def main():
+    # Wait until we get a successful response from the World Time API.
+    failed = 1
+    while failed:
+        failed, response = commands.getstatusoutput('curl -s http://worldtimeapi.org/api/timezone/America/Lima')
 
-shell_cmd = date_obj.strftime('sudo date -s "%a %b %d %X -05 %Y"')
-os.system(shell_cmd)
+    # Parse JSON string into a dictionary.
+    api_data = json.loads(response)
+
+    # Fetch the datetime data from the dictionary and build a datetime object.
+    date_str = api_data['datetime']
+    date_fmt = '%Y-%m-%dT%X.%f-05:00'
+    date_obj = datetime.strptime(date_str, date_fmt)
+
+    # Construct the shell command to update the system time and execute it.
+    shell_cmd = date_obj.strftime('sudo date -s "%a %b %d %X -05 %Y"')
+    os.system(shell_cmd)
+
+
+if __name__ == '__main__':
+    main()
